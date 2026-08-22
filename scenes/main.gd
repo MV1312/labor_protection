786ec5ea804f1a8d251758ad0ad6a559eee295b2
Vlad_M <mr.mvkrizi@gmail.com>
2026-08-22 11:59:26 +0300

    refactoring bags

---
+++ scenes/main.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/questions_ 2.tscn")
+
+
+func _on_info_pressed() -> void:
+	pass # Replace with function body.
