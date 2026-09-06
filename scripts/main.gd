9e05b00d3676b4f97944a8882a31011ec23ff9be
Vlad_M <mr.mvkrizi@gmail.com>
2026-09-06 10:09:27 +0300

    Removing incorrect modules and adding a separate scene for the button with information.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,3 @@ extends Control
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/questions_2.tscn")
-
-
-func _on_info_pressed() -> void:
-	pass # Replace with function body.
