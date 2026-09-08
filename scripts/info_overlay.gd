42fa9561f3e48456ea3bbef4ce3a3a3757400b89
Vlad_M <mr.mvkrizi@gmail.com>
2026-09-08 10:24:51 +0300

    Changing the structure of the window for viewing information, refining the code of this window, and rewriting the release.

--- scripts/info_overlay.gd
+++ scripts/info_overlay.gd
@@ -1,11 +1,24 @@
-
 extends CanvasLayer
 
 @onready var info_button: Button = $InfoButton
 @onready var info_window: PanelContainer = $InfoWindow
 @onready var tap_to_close: Control = $TapToClose
 @onready var dim_background: ColorRect = $DimBackground
+@onready var info_text: RichTextLabel = $InfoWindow/ScrollContainer/InfoText # $InfoWindow/MarginContainer/ScrollContainer/InfoText
 
+# Можно хранить текст в ресурсе или файле
+var info_content: String = """
+[center][b]ОБ ИГРЕ[/b][/center]
+
+[color=yellow]Основные механики:[/color]
+• Двигайтесь с помощью свайпов
+• Собирайте бонусы
+• Избегайте препятствий
+
+[color=green]Совет:[/color] [i]Попробуйте найти все секреты![/i]
+
+[color=gray]Версия 1.0.0[/color]
+"""
 
 func _ready() -> void:
 	info_button.toggle_mode = true
@@ -14,22 +27,15 @@ func _ready() -> void:
 	tap_to_close.gui_input.connect(_on_tap_to_close_gui_input)
 	tap_to_close.mouse_filter = Control.MOUSE_FILTER_STOP
 
+	# Загрузка контента (можно из отдельного файла)
+	_load_info_content()
+	
 	_set_info_visible(false)
 
 
 func _on_info_button_toggled(button_pressed: bool) -> void:
 	_set_info_visible(button_pressed)
 
-
-func _set_info_visible(visible_state: bool) -> void:
-	info_window.visible = visible_state
-	tap_to_close.visible = visible_state
-	dim_background.visible = visible_state
-
-	if visible_state:
-		info_button.text = "Скрыть"
-	else:
-		info_button.text = "Инфо"
 
 
 func _on_tap_to_close_gui_input(event: InputEvent) -> void:
@@ -51,3 +57,27 @@ func _is_touch_or_click(event: InputEvent) -> bool:
 func close_info() -> void:
 	info_button.set_pressed_no_signal(false)
 	_set_info_visible(false)
+
+
+func _load_info_content() -> void:
+	# Вариант 1: Прямой текст
+	info_text.text = info_content
+	
+	# Вариант 2: Из JSON файла (будущее масштабирование)
+	# var data = load_info_from_json("res://data/info.json")
+	# info_text.text = data["content"]
+	
+func _set_info_visible(visible_state: bool) -> void:
+	info_window.visible = visible_state
+	tap_to_close.visible = visible_state
+	dim_background.visible = visible_state
+	
+	# Сброс скролла при открытии
+	if visible_state:
+		var scroll = info_window.get_node("ScrollContainer") #MarginContainer/ScrollContainer")
+		scroll.scroll_vertical = 0
+		info_button.text = "Скрыть"
+	else:
+		info_button.text = "Инфо"
+		
+		
